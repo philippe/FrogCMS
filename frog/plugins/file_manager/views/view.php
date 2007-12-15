b4ad463b3b69968f34a27ca555ee0486897768a1
philippe.archambault <philippe.archambault@f68477e9-1835-0410-8f7c-e127d972d001>
2007-12-15 17:44:33 +0000

    class buttons was missing on plugin file_manager view.php

--- frog/plugins/file_manager/views/view.php
+++ frog/plugins/file_manager/views/view.php
@@ -23,7 +23,7 @@
             <textarea class="textarea" id="file_content" name="file[content]" style="width: 100%; height: 400px;" onkeydown="return allowTab(event, this);" rows="20" cols="40"><?php echo htmlentities($content, ENT_COMPAT, 'UTF-8'); ?></textarea><br />
         </p>
     </div>
-    <p>
+    <p class="buttons">
         <input class="button" name="commit" type="submit" accesskey="s" value="<?php echo __('Save'); ?>" />
         <input class="button" name="continue" type="submit" accesskey="e" value="<?php echo __('Save and Continue Editing'); ?>" />
         <?php echo __('or'); ?> <a href="<?php echo get_url('plugin/file_manager/browse/'.$progres_path); ?>"><?php echo __('Cancel'); ?></a>
