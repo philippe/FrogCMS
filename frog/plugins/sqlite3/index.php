ac228fd57428cf9ebefd69a1e801ffdc93fc101e
philippe.archambault <philippe.archambault@f68477e9-1835-0410-8f7c-e127d972d001>
2008-01-16 19:14:26 +0000

    bug fixed: sqlite 3 plugin crash administration setting when PDO is not installed

--- frog/plugins/sqlite3/index.php
+++ frog/plugins/sqlite3/index.php
@@ -1,23 +1,31 @@
 <?php
 
-Plugin::setInfos(array(
-    'id'          => 'sqlite3',
-    'title'       => 'SQLite 3', 
-    'description' => 'Provides function to run Frog CMS with SQLite 3 database.', 
-    'version'     => '1.0', 
-    'website'     => 'http://www.madebyfrog.com/')
-);
-
-// adding function date_format to sqlite 3 'mysql date_format function'
-if (! function_exists('mysql_date_format_function'))
+try
 {
-    function mysql_function_date_format($date, $format)
+    if (class_exists('PDO'))
     {
-        return strftime($format, strtotime($date));
-    }
-}
+        Plugin::setInfos(array(
+            'id'          => 'sqlite3',
+            'title'       => 'SQLite 3', 
+            'description' => 'Provides function to run Frog CMS with SQLite 3 database.', 
+            'version'     => '1.0', 
+            'website'     => 'http://www.madebyfrog.com/')
+        );
 
-if (isset($GLOBALS['__FROG_CONN__']) && $GLOBALS['__FROG_CONN__']->getAttribute(PDO::ATTR_DRIVER_NAME) == 'sqlite')
-    $GLOBALS['__FROG_CONN__']->sqliteCreateFunction('date_format', 'mysql_function_date_format', 2);
-else if (Record::getConnection()->getAttribute(Record::ATTR_DRIVER_NAME) == 'sqlite')
-    Record::getConnection()->sqliteCreateFunction('date_format', 'mysql_function_date_format', 2);
+        // adding function date_format to sqlite 3 'mysql date_format function'
+        if (! function_exists('mysql_date_format_function'))
+        {
+            function mysql_function_date_format($date, $format)
+            {
+                return strftime($format, strtotime($date));
+            }
+        }
+        
+        if (isset($GLOBALS['__FROG_CONN__']) && $GLOBALS['__FROG_CONN__']->getAttribute(PDO::ATTR_DRIVER_NAME) == 'sqlite')
+            $GLOBALS['__FROG_CONN__']->sqliteCreateFunction('date_format', 'mysql_function_date_format', 2);
+        else if (Record::getConnection()->getAttribute(Record::ATTR_DRIVER_NAME) == 'sqlite')
+            Record::getConnection()->sqliteCreateFunction('date_format', 'mysql_function_date_format', 2);
+    }
+} catch (Exception $e) {
+    // nothing to do ... if PDO do not exist the plugin will not be used
+}
