4337e81f5db8abfecd8945ce64151dceb388c828
philippe.archambault <philippe.archambault@f68477e9-1835-0410-8f7c-e127d972d001>
2008-03-04 14:44:30 +0000

    Add italien and hungarian translations

---
+++ frog/app/backend/i18n/hu-message.php
@@ -0,0 +1,219 @@
+<?php
+
+/**
+ * this file has been translated by: H.S.Laszlo http://www.bluedesign.hu
+ */
+
+return array(
+
+// general
+'Pages' => 'Oldalak',
+'Snippets' => 'Modulok',
+'Layouts' => 'Layout-ok',
+'Files' => 'Fájlok',
+'Users' => 'Felhasználók',
+'Modify' => 'Módosítás',
+'Action' => 'Művelet',
+'Close' => 'Bezár',
+'Save' => 'Mentés',
+'Save and Continue Editing' => 'Ment és tovább szerkeszt',
+'or' => 'vagy',
+'Cancel' => 'Mégsem',
+'Name' => 'Név',
+'Last updated by' => 'Utoljára frissítette',
+'on' => 'on',
+'Upload' => 'Feltölt',
+'Are you sure you wish to delete' => 'Biztos, hogy törölni akarod?',
+'Published' => 'Publikálva',
+'Draft' => 'Vázlat',
+'Reviewed' => 'Ellenőrizve',
+'Hidden' => 'Rejtett',
+'More' => 'Több',
+'Less' => 'Kevesebb',
+'Add' => 'Hozzáad',
+'Required.' => 'Szükséges.',
+'Optional.' => 'Opcionális.',
+'yes' => 'igen',
+'no' => 'nem',
+'Create' => 'Létrehoz',
+
+// files
+'File' => 'Fájl',
+'Size' => 'Méret',
+'Permissions' => 'Attribútumok',
+'Change mode' => 'Change mode',
+'Remove file' => 'Fájl eltávolítása',
+'Create new file' => 'Új fájl létrehozása',
+'Create new directory' => 'Új könyvtár létrehozása',
+'Upload file' => 'Fájl feltöltése',
+'Rename' => 'Átnevezés',
+'overwrite it?' => 'felülírod?',
+
+// layouts
+'Layout' => 'Layout',
+'New Layout' => 'Új Layout',
+'What is a Layout?' => 'Mi a Layout?',
+'Use layouts to apply a visual look to a Web page. Layouts can contain special tags to include
+  page content and other elements such as the header or footer. Click on a layout name below to
+  edit it or click <strong>Remove</strong> to delete it.' =>
+'Használd arra az alaprajzokat, hogy alkalmazzanak egy vizuális pillantást egy weblapba. Az alaprajzok különleges címkéket tartalmazhatnak tartalmazni
+hívj elégedett és másik elemeket, mint például a fejlécet vagy a lábjegyzetet. Lent kattint egy alaprajznéven
+szerkeszd azt vagy kattints <strong>Remove</strong> hogy töröld azt.',
+'Add Layout' => 'Új Layout',
+'Edit Layout' => 'Layout szerkesztése',
+'Remove Layout' => 'layout eltávolítása',
+'inherit' => 'örököl',
+
+// pages
+'Page' => 'Oldal',
+'reorder' => 'Újrarendezés',
+'Add Page' => 'Oldal hozzáadása',
+'Edit Page' => 'Oldal szerkesztése',
+'Status' => 'Állapot',
+'Order' => 'Elrendezés',
+'Add child' => 'Aloldal hozzáadása',
+'Add Tab' => 'Tab hozzáadása',
+'Add Part' => 'Rész hozzáadása',
+'Remove Tab' => 'Tab eltávolítása',
+'Delete the current tab?' => 'Töröljem az aktuális Tab-ot?',
+'Remove page' => 'Oldal eltávolítása',
+'Remove page disable' => 'Oldal eltávolításának tiltása',
+'Drag and Drop' => 'Fogd és vidd',
+'Drag and Drop disable' => 'Fogd és vidd tiltása',
+'Page Title' => 'Oldal címe',
+'Metadata' => 'Meta adatok',
+'Filter' => 'Szűrő',
+'none' => 'nem',
+'Page Type' => 'Oldal típus',
+'Comments' => 'Hozzászólások',
+
+// settings
+'Administration' => 'Beállítások',
+'Settings' => 'Beállítások',
+'Admin Site title' => 'Admin oldal címe',
+'Language' => 'Nyelv',
+'Administration Theme' => 'Admin oldal stílusa',
+'Page options' => 'Oldal opciók',
+'Default Status' => 'Alapértelmezett állapot',
+'Default Filter' => 'Alapértelmezett szűrő',
+'Optional component' => 'Opcionális komponensek',
+'Enable comments' => 'Kommentek bekapcsolása',
+'Auto approve comments' => 'Kommentek automatikus jóváhagyása',
+'Display stats' => 'Statisztika megjelenítése',
+'Display file manager' => 'File kezelő megjelenítése',
+'By using <strong>&lt;img src="img_path" /&gt;</strong> you can set your company logo instead of a title.' => 'A <strong>&lt;img src="img_path" /&gt;</strong> használatával lehet logót beilleszteni a fejlécbe',
+'This will set your language for the backend.' => 'Szerkesztő felület nyelve',
+'This will change your Administration theme.' => 'Szerkesztő felület grafikus stílusa',
+'Only for filter in pages, NOT in snippets' => 'Szűrő, csak az oldalak tartalmához!',
+
+// snippets
+'New Snippet' => 'Új Modul',
+'What is a Snippet?' => 'Mi az a Modul?',
+'Snippets are generally small pieces of content which are included in other pages or layouts.' =>
+'A Modulok kis kódrészletek, melyeket bárhova be lehet szúrni, oldalakba, vagy Layout-okba.',
+'Snippet' => 'Modul',
+'Add Snippet' => 'Új Modul',
+'Edit Snippet' => 'Modul szerkesztése',
+'Remove Snippet' => 'Modul törlése',
+
+
+// users
+'Add User' => 'Felhasználó hozzáadása',
+'Edit user' => 'Felhasználó szerkesztése',
+'Username' => 'Felhasználói név',
+'E-mail' => 'E-mail',
+'Password' => 'Jelszó',
+'Forgot password?' => 'Elfelejtett jelszó?',
+'Forgot password' => 'Elfelejtett jelszó',
+'Email address:' => 'E-mail cím:',
+'Confirm Password' => 'Jelszó megerősítése',
+'Send password' => 'Jelszó elküldése',
+'No user found!' => 'Nincs ilyen felhasználó!',
+'Login' => 'Belépés',
+'Remember me for 14 days' => 'Jegyezz meg 14 napig',
+'Roles' => 'Szerepkör',
+'Administrator' => 'Adminisztrátor',
+'Developer' => 'Fejlesztő',
+'Editor' => 'Szerkesztő',
+'Optional. Please use a valid e-mail address.' => 'Nem kötelező. Létező e-mail címet adj meg.',
+'At least 3 characters. Must be unique.' => 'Legalább 3 karakter. Egyedinek kell lennie.',
+'At least 5 characters.' => 'Legalább 5 karakter.',
+'Leave password blank for it to remain unchanged.' => 'Hagyd üresen a jelszó mezőt, hogy változatlan maradjon.',
+'Roles restrict user privileges and turn parts of the administrative interface on or off.' => 'A szerepkörök különböző mértékű hozzáférést tesznek lehetővé az adminisztrációs felülethez.',
+'New User' => 'Új felhasználó',
+'Where do the avatars come from?' => 'Honnét lesz avatarom?',
+'The avatars are automatically linked for those with a <a href="http://www.gravatar.com/" target="_blank">Gravatar</a> (a free service) account.' => 'Direkt linkelve a <a href="http://www.gravatar.com/" target="_blank">Gravatar</a>.com oldalról. Ez ingyenes!',
+
+
+
+//'File :name has been created with success!' => 'Le fichier <strong>:name</strong> a été créé avec succès!',
+//'Directory is not writable! File has not been saved!' => 'Vous ne détenez pas les droits d\'écriture sur le répertoire. Le fichier na pas été enregistré',
+//'File :name has been created!' => 'Le fichier <strong>:name</strong> a été créé!',
+//'File :name has not been created!' => 'Le fichier <strong>:name</strong> n\'a pas été créé!',
+//'Directory :name has been created!' => 'Le répertoire <strong>:name</strong> a été créé!',
+//'Directory :name has not been created!' => 'Le répertoire <strong>:name</strong> n\'a pas été créé!',
+//'File :name has been deleted with success!' => 'Le fichier <strong>:name</strong> a été supprimé avec succès!',
+//'File :name has not been deleted!' => 'Le fichier <strong>:name</strong> n\'a pas été supprimé!',
+//'Directory :name has been deleted with success!' => 'Le répertoire <strong>:name</strong> a été supprimé avec succès!',
+//'Directory :name has not been deleted!' => 'Le répertoire <strong>:name</strong> n\'a pas été supprimé!',
+//'File :name has been uploaded with success!' => 'Le fichier <strong>:name</strong> a été téléchargé avec succès!',
+//'File has not been uploaded!' => 'Le fichier n\'a pas été téléchargé!',
+//'Permissions of file has been changed!' => 'Les permissions sur le fichier ont été modifiées!',
+//'Permissions of directory has been changed!' => 'Les permissions sur le répertoire on été modifiées!',
+//'Change mode has not been done!' => 'Le changement de permission n\'a pas fonctionné!',
+//'File or directory not found!' => 'Le fichier ou le répertoire n\'a pas été trouvé!',
+
+// errors and success
+'Layout has been added!' => 'Layout létrehozva!',
+'Layout has not been added. Name must be unique!' => 'A Layout nem lett létrehozva, mert ilyen nevű már létezik!',
+'Layout has been saved!' => 'Layout elmentve!',
+'Layout has not been saved. Name must be unique!' => 'A Layout nem lett elmentve, mert ilyen nevű már létezik!',
+'Layout :name has been deleted!' => 'A <strong>:name</strong> nevű Layout törölve lett!',
+'Layout :name has not been deleted!' => 'A <strong>:name</strong> nevű Layout nem törlődött!',
+'Layout not found!' => 'A Layout nem található!',
+
+'Login failed. Please check your login data and try again.' => 'Login failed. Please check your login data and try again.',
+
+'Page has been saved!' => 'Oldal létehozva!',
+'Page has not been saved!' => 'Oldal nem lett létehozva!',
+'Page not found!' => 'Oldalt nem találom!',
+'Page :title as been deleted!' => 'Oldal <strong>:title</strong> as been deleted!',
+'Page :title as not been deleted!' => 'Page <strong>:title</strong> as not been deleted!',
+'Action disabled!' => 'Tevékenység disabled!',
+
+'Snippet has been added!' => 'Snippet létrehozva!',
+'Snippet has not been added. Name must be unique!' => 'Snippet has not been added. Name must be unique!',
+'Snippet :name has been saved!' => 'Snippet <strong>:name</strong> elmentve!',
+'Snippet :name has not been saved. Name must be unique!' => 'Snippet <strong>:name</strong> has not been saved. Name must be unique!',
+'Snippet :name has been deleted!' => 'Snippet <strong>:name</strong> törölve!',
+'Snippet :name has not been deleted!' => 'Snippet <strong>:name</strong> nem lett törölve!',
+'Snippet not found!' => 'Snippet not found!',
+
+'You do not have permission to access the requested page!' => 'Nincs jogod az oldalra belépni!',
+'Password and Confirm are not the same or too small!' => 'Jelszó és a megerősítése nem egyezik, vagy túl rövid!',
+'Username must be 3 character minimum!' => 'Felhasználói név minimum 3 karakter!',
+'User has been added!' => 'Felhasználó létrehozva!',
+'User has not been added!' => 'Felhasználó nem lett létrehozva!',
+'User not found!' => 'Felhasználó nem található!',
+'Username have not been save must be 3 character minimum!' => 'Nem lett mentve, mert a felhasználói név minimum 3 karakter!',
+'User has been saved!' => 'Felhasználó mentve!',
+'User has not been saved!' => 'Felhasználó nem lett mentve!',
+'User <strong>:name</strong> has been deleted!' => 'Felhaználó <strong>:name</strong> törölve!',
+'User <strong>:name</strong> has not been deleted!' => 'Felhasználó <strong>:name</strong> nem lett törölve!',
+'Settings has been saved!' => 'Beállítások mentve!',
+
+// layout
+'You are currently logged in as' => 'Belépve, mint ',
+'Log Out' => 'Kilépés',
+'View Site' => 'Oldal megtekintése',
+'Thank you for using' => 'Köszönjük, hogy a szerkesztéshez rendszerünket használta',
+'version' => 'verzió',
+'Page rendered in' => 'Oldal megváltoztatva',
+'seconds' => 'másodperc',
+'Memory usage:' => 'Memória használat:',
+
+// Comments
+'Delete' => 'Törlés',
+'Reject' => 'Elvet',
+
+);
