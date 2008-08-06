8467b9abbf075d4874422a656bc7ea31245e60ea
philippe.archambault <philippe.archambault@f68477e9-1835-0410-8f7c-e127d972d001>
2008-08-06 14:53:36 +0000

    i18n for filemanager plugin - fr

---
+++ frog/plugins/file_manager/i18n/fr-message.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * this file has been translated by: Philippe Archambault
+ */
+
+return array(
+
+// files
+'File' => 'Fichier',
+'Size' => 'Taille',
+'Permission' => 'Permission',
+'Change mode' => 'Changer le mode',
+'Remove file' => 'Supprimer le fichier',
+'Create new file' => 'Créer un fichier',
+'Create new directory' => 'Créer un répertoire',
+'Upload file' => 'Télécharger un fichier',
+'rename' => 'renommer',
+
+'File :name has been created with success!' => 'Le fichier <strong>:name</strong> a été créé avec succès!',
+'Directory is not writable! File has not been saved!' => 'Vous ne détenez pas les droits d\'écriture sur le répertoire. Le fichier na pas été enregistré',
+'File :name has been created!' => 'Le fichier <strong>:name</strong> a été créé!',
+'File :name has not been created!' => 'Le fichier <strong>:name</strong> n\'a pas été créé!',
+'Directory :name has been created!' => 'Le répertoire <strong>:name</strong> a été créé!',
+'Directory :name has not been created!' => 'Le répertoire <strong>:name</strong> n\'a pas été créé!',
+'File :name has been deleted with success!' => 'Le fichier <strong>:name</strong> a été supprimé avec succès!',
+'File :name has not been deleted!' => 'Le fichier <strong>:name</strong> n\'a pas été supprimé!',
+'Directory :name has been deleted with success!' => 'Le répertoire <strong>:name</strong> a été supprimé avec succès!',
+'Directory :name has not been deleted!' => 'Le répertoire <strong>:name</strong> n\'a pas été supprimé!',
+'File :name has been uploaded with success!' => 'Le fichier <strong>:name</strong> a été téléchargé avec succès!',
+'File has not been uploaded!' => 'Le fichier n\'a pas été téléchargé!',
+'Permissions of file has been changed!' => 'Les permissions sur le fichier ont été modifiées!',
+'Permissions of directory has been changed!' => 'Les permissions sur le répertoire on été modifiées!',
+'Change mode has not been done!' => 'Le changement de permission n\'a pas fonctionné!',
+'File or directory not found!' => 'Le fichier ou le répertoire n\'a pas été trouvé!',
+
+);
