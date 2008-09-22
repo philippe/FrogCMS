3f06115a78288db3d6e3706b4238cadac4faa934
philippe.archambault <philippe.archambault@f68477e9-1835-0410-8f7c-e127d972d001>
2008-09-22 21:04:01 +0000

    adding snippet code to sidebar

--- frog/app/backend/views/snippet/sidebar.php
+++ frog/app/backend/views/snippet/sidebar.php
@@ -6,5 +6,10 @@
     <h2><?php echo __('What is a Snippet?'); ?></h2>
     <p><?php echo __('Snippets are generally small pieces of content which are included in other pages or layouts.'); ?></p>
 </div>
+<div class="box">
+    <h2><?php echo __('Tag to use this snippet'); ?></h2>
+    <p><?php echo __('Just replace <b>snippet</b> by the snippet name you want to include.'); ?></p>
+    <p><code>&lt;?php $this->includeSnippet('snippet'); ?&gt;</code></p>
+</div>
 
 <?php endif; ?>
