5bbb9c1d33decb0907cc7f3b6475e9a0fb312170
martijn.niji@gmail.com <martijn.niji@gmail.com@f68477e9-1835-0410-8f7c-e127d972d001>
2009-04-26 11:25:03 +0000

    Added exit()

--- frog/app/layouts/backend.php
+++ frog/app/layouts/backend.php
@@ -1,4 +1,4 @@
-<?php if (!AuthUser::hasPermission('administrator,developer,editor')) header('Location: '.URL_PUBLIC.' '); ?>
+<?php if (!AuthUser::hasPermission('administrator,developer,editor')) header('Location: '.URL_PUBLIC.' '); exit(); ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
