132fb8c3f646db53d86521f174eff56308048b21
djreimer <djreimer@f68477e9-1835-0410-8f7c-e127d972d001>
2009-05-12 08:48:24 +0000

    Added Slovak translations. Thanks Payo.

---
+++ frog/plugins/file_manager/i18n/sk-message.php
@@ -0,0 +1,45 @@
+<?php
+
+  /**
+     * Slovak file for plugin file_manager
+     *
+     * @package frog
+     * @subpackage plugin.file_manager.translations
+     *
+     * @author Pali Petras <payo@creoactive.com>
+     * @version Frog 0.9.5
+     */
+
+    return array(
+    'A possible hack attempt was detected.' => 'Bol zaznamenaný možný pokus o hack.',
+	'Action' => 'Akcia',
+	'Are you sure you wish to delete' => 'Naozaj si želáte zmazať?',
+	'Cancel' => 'Zrušiť',
+	'Change mode' => 'Upraviť mód',
+	'Close' => 'Zavrieť',
+	'Create' => 'Vytvoriť',
+	'Create new directory' => 'Vytvoriť nový adresár',
+	'Create new file' => 'Vytvoriť nový súbor',
+	'Directory :name has not been created!' => 'Adresár <strong>:name</strong> nebol vytvorený!',
+	'Directory is not writable! File has not been saved!' => 'Nie je možné zapisovať do adresára. Súbor nebol uložený.',
+	'File' => 'Súbor',
+	'File :name has been created with success!' => 'Súbor<strong>:name</strong> bol vytvorený!',
+	'File :name has not been created!' => 'Súbor <strong>:name</strong> nebol vytvorený!',
+	'File has been saved with success!' => 'Súbor bol úspešne uložený!',
+	'File has not been uploaded!' => 'Súbor nebol odoslaný!',
+	'File is not writable! File has not been saved!' => 'Nie je možné zapisovať do súboru. Súbor nebol uložený!',
+	'File or directory not found!' => 'Súbor alebo adresár nenájdený!',
+	'Modify' => 'Zmeniť',
+	'Permission denied!' => 'Prístup odmietnutý!',
+	'Permissions' => 'Práva',
+	'Rename' => 'Premenovať',
+	'Save' => 'Uložiť',
+	'Save and Continue Editing' => 'Uložiť a pokračovať v úpravách',
+	'Size' => 'Veľkosť',
+	'Upload' => 'Odoslať',
+	'Upload file' => 'Odoslať súbor',
+	'You do not have permission to access the requested page!' => 'Nemáte prístupové oprávnenie na stránku!',
+	'or' => 'alebo',
+	'overwrite it?' => '&nbsp;Prepísať?',
+    );
+	
---
+++ frog/plugins/skeleton/i18n/sk-message.php
@@ -0,0 +1,20 @@
+<?php
+
+  /**
+     * Slovak file for plugin skeleton
+     *
+     * @package frog
+     * @subpackage plugin.skeleton.translations
+     *
+     * @author Pali Petras <payo@creoactive.com>
+     * @version Frog 0.9.5
+     */
+
+    return array(
+        'A sidebar' => 'Bočný panel',
+    	'Display settings page here!' => 'Zobraziť nastavenia stránky!',
+        'Display your documentation here!' => 'Zobraziť dokumentáciu!',
+        'Documentation' => 'Dokumentácia',
+        'Put something here, or leave out the sidebar entirely.' => 'Niečo vložte alebo vynechajte celý bočný panel.',
+        'Settings' => 'Nastavenia',
+    );
