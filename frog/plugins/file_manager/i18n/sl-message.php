bcd04e94bf4b372ef4c449005503a7e5d75ee160
djreimer <djreimer@f68477e9-1835-0410-8f7c-e127d972d001>
2009-06-15 09:51:55 +0000

    Added Slovenian translations. Thanks skye28.

---
+++ frog/plugins/file_manager/i18n/sl-message.php
@@ -0,0 +1,43 @@
+<?php
+
+    /**
+     * Slovenian file for plugin file_manager
+     *
+     * @package frog
+     * @subpackage plugin.file_manager.translations
+     *
+     * @author Andrej Čremožnik <andrej.cremoznik@gmail.com>
+     * @version Frog 0.9.5
+     */
+
+    return array(
+    	'A possible hack attempt was detected.' => 'Zaznan je bil poskus vdora.',
+	'Action' => 'Urejanje',
+	'Are you sure you wish to delete' => 'Ali ste prepričani, da želite izbrisati',
+	'Cancel' => 'Prekliči',
+	'Change mode' => 'Spremeni dovoljenja',
+	'Close' => 'Zapri',
+	'Create' => 'Ustvari',
+	'Create new directory' => 'Ustvari novo mapo',
+	'Create new file' => 'Ustvari novo datoteko',
+	'Directory :name has not been created!' => 'Mapa :name ni bila ustvarjena!',
+	'Directory is not writable! File has not been saved!' => 'Mapa ni zapisljiva! Datoteka ni bila shranjena!',
+	'File' => 'Datoteka',
+	'File :name has been created with success!' => 'Datoteka :name je bila uspešno ustvarjena!',
+	'File :name has not been created!' => 'Datoteka :name ni bila ustvarjena!',
+	'File has been saved with success!' => 'Datoteka je bila uspešno shranjena!',
+	'File has not been uploaded!' => 'Datoteka ni bila naložena!',
+	'File is not writable! File has not been saved!' => 'Datoteka ni zapisljiva! Spremembe niso bile shranjene!',
+	'File or directory not found!' => 'Datoteke ali mape ni moč najti!',
+	'Permission denied!' => 'Ni dovoljenja!',
+	'Permissions' => 'Dovoljenja',
+	'Rename' => 'Preimenuj',
+	'Save' => 'Shrani',
+	'Save and Continue Editing' => 'Shrani in nadaljuj z urejanjem',
+	'Size' => 'Velikost',
+	'Upload' => 'Naloži',
+	'Upload file' => 'Naloži datoteko',
+	'You do not have permission to access the requested page!' => 'Nimate dovoljenj za dostop do zahtevane strani!',
+	'or' => 'ali',
+	'overwrite it?' => 'prepiši?',
+    );
---
+++ frog/plugins/skeleton/i18n/sl-message.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+     * Slovenian file for plugin skeleton
+     *
+     * @package frog
+     * @subpackage plugin.skeleton.translations
+     *
+     * @author Andrej Čremožnik <andrej.cremoznik@gmail.com>
+     * @version Frog 0.9.5
+     */
+
+    return array(
+    	'A sidebar' => 'Stranski stolpec',
+	'Display settings page here!' => 'Tukaj prikaži stran z nastavitvami!',
+	'Display your documentation here!' => 'Tukaj prikaži dokumentacijo!',
+	'Documentation' => 'Dokumentacija',
+	'Put something here, or leave out the sidebar entirely.' => 'Vstavi nekaj sem, ali pa povsem izpusti stranski stolpec.',
+	'Settings' => 'Nastavitve',
+    );
